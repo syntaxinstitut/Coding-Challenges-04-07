c0ad91d22a0f6a07a466510f988e94ff45aa55ed
Fabian Moreno <fabian.moreno@syntax-institut.de>
2025-03-31 09:04:12 +0200

    more Imports

--- app/src/main/java/de/syntax_institut/codingchallenges04_07/MainActivity.kt
+++ app/src/main/java/de/syntax_institut/codingchallenges04_07/MainActivity.kt
@@ -1,10 +1,10 @@
 package de.syntax_institut.codingchallenges04_07
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,12 +16,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import de.syntax_institut.codingchallenges04_07.ui.theme.CodingChallenges0407Theme
 import androidx.compose.runtime.getValue
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 
 
 class MainActivity : ComponentActivity() {
